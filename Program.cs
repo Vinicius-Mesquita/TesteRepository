8b9db6264a4dac7f304f48f7b845a0b6b8c3bd12
Vinicius <vinicius.junge@selbetti.com.br>
2024-12-18 14:36:25 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace console_app
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            
+        }
+
+        public static void tabuada(int numero)
+        {
+        for(int i = 1; i <= 10 ; i++)
+            {
+                Console.WriteLine(numero + "X" + i + "=" + (numero*i));
+            }
+        }
+    }
+}
+
+
+
+
